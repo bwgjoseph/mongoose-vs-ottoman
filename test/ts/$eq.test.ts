198b8262c463f06c5b8683ec6eb3ce6d07d5d0de
bwgjoseph <josephgan@live.com.sg>
2020-12-04 13:16:37 +0800

    verify eq and neq operator
    Close #20

--- test/ts/$eq.test.ts
+++ test/ts/$eq.test.ts
@@ -1,8 +1,8 @@
 import assert from 'assert';
 import { SearchConsistency } from 'ottoman';
-import { removeDocuments } from './setup/util';
 import { falcon, hawk } from './setup/fixtures';
 import { getMongooseModel, getOttomanModel } from './setup/model';
+import { removeDocuments } from './setup/util';
 
 describe('test $eq function', async () => {
     it('mongoose - simple $eq should be able to work', async () => {
@@ -21,6 +21,7 @@ describe('test $eq function', async () => {
         assert.strictEqual(find[0].name, 'Couchbase Airlines');
     });
 
+    // @fixed: 2.0.0-alpha.10
     it('ottoman - simple $eq should be able to work', async () => {
         const Airplane = getOttomanModel();
         const hawkAirplane = new Airplane(hawk);
@@ -28,7 +29,6 @@ describe('test $eq function', async () => {
         await Airplane.create(falconAirplane);
         await Airplane.create(hawkAirplane);
 
-        //$eq for ottoman works for number but not string.
         const find = await Airplane.find({
             capacity : {
                 $eq : 250
@@ -48,11 +48,11 @@ describe('test $eq function', async () => {
         {
             consistency: SearchConsistency.LOCAL
         });
-        // Bug: https://github.com/bwgjoseph/mongoose-vs-ottoman/issues/20, https://github.com/couchbaselabs/node-ottoman/pull/313
+
         assert.strictEqual(find2.rows.length, 1);
         assert.strictEqual(find2.rows[0].name, 'Couchbase Airlines');
 
         await removeDocuments();
-        
+
     });
 });
--- test/ts/$ne.test.ts
+++ test/ts/$ne.test.ts
@@ -1,8 +1,8 @@
 import assert from 'assert';
 import { SearchConsistency } from 'ottoman';
-import { removeDocuments } from './setup/util';
 import { falcon, hawk } from './setup/fixtures';
 import { getMongooseModel, getOttomanModel } from './setup/model';
+import { removeDocuments } from './setup/util';
 
 describe('test $ne function', async () => {
     it('mongoose - simple $ne should be able to work', async () => {
@@ -22,6 +22,7 @@ describe('test $ne function', async () => {
 
     });
 
+    // @fixed: 2.0.0-alpha.10
     it('ottoman - simple $neq should be able to work', async () => {
         const Airplane = getOttomanModel();
         const hawkAirplane = new Airplane(hawk);
@@ -29,7 +30,6 @@ describe('test $ne function', async () => {
         await Airplane.create(falconAirplane);
         await Airplane.create(hawkAirplane);
 
-        //$neq for ottoman works for number but not string.
         const find = await Airplane.find({
             capacity : {
                 $neq : 250
@@ -41,7 +41,6 @@ describe('test $ne function', async () => {
         assert.strictEqual(find.rows.length, 1);
         assert.strictEqual(find.rows[0].capacity, 550);
 
-
         const find2 = await Airplane.find({
             name : {
                 $neq : 'Couchbase Airlines'
@@ -50,7 +49,6 @@ describe('test $ne function', async () => {
         {
             consistency: SearchConsistency.LOCAL
         });
-        // Bug: https://github.com/bwgjoseph/mongoose-vs-ottoman/issues/20, https://github.com/couchbaselabs/node-ottoman/pull/313
         assert.strictEqual(find2.rows.length, 1);
         assert.strictEqual(find2.rows[0].name, 'Mongo Airlines');
 
