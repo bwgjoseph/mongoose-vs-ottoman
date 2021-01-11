2a4d0a3320ef44c2f9f74ea2458c4c45610dcaaa
bwgjoseph <josephgan@live.com.sg>
2021-01-11 23:57:29 +0800

    update removeMany test
    Reference #5

--- test/ts/removeMany.test.ts
+++ test/ts/removeMany.test.ts
@@ -9,6 +9,8 @@ describe('test removeMany function', async () => {
         const Airplane = getMongooseModel();
         const hawkAirplane = new Airplane(hawk);
         await Airplane.create(hawkAirplane);
+        const findbefore = await Airplane.find().exec();
+        assert.strictEqual(findbefore.length, 1);
 
         await Airplane.deleteMany({
             name: 'Couchbase Airlines'
@@ -22,13 +24,65 @@ describe('test removeMany function', async () => {
         const hawkAirplane = new Airplane(hawk);
         const eagleAirplane = new Airplane(eagle);
         await Airplane.create(hawkAirplane);
-        await Airplane.create(eagleAirplane); 
+        await Airplane.create(eagleAirplane);
+        const findbefore = await Airplane.find({}, {
+            consistency: SearchConsistency.LOCAL
+        });
+        assert.strictEqual(findbefore.rows.length, 2);
+
+        // remove all docs with name: Couchbase
+        await Airplane.removeMany({
+            name: 'Couchbase Airlines'
+        },
+        {
+            consistency: SearchConsistency.LOCAL
+        });
+        const find = await Airplane.find();
+        assert.strictEqual(find.rows.length, 0);
+
+        await removeDocuments();
+    });
+
+    it('ottoman - should remove many docs using $like', async () => {
+        const Airplane = getOttomanModel();
+        const hawkAirplane = new Airplane(hawk);
+        const eagleAirplane = new Airplane(eagle);
+        await Airplane.create(hawkAirplane);
+        await Airplane.create(eagleAirplane);
+        const findbefore = await Airplane.find({}, {
+            consistency: SearchConsistency.LOCAL
+        });
+        assert.strictEqual(findbefore.rows.length, 2);
 
         // remove all docs with name: Couchbase
         await Airplane.removeMany({
             name: {
                 $like: '%Couchbase%'
             }
+        },
+        {
+            consistency: SearchConsistency.LOCAL
+        });
+        const find = await Airplane.find();
+        assert.strictEqual(find.rows.length, 0);
+
+        await removeDocuments();
+    });
+
+    it('ottoman - should NOT remove many docs (does not match)', async () => {
+        const Airplane = getOttomanModel();
+        const hawkAirplane = new Airplane(hawk);
+        const eagleAirplane = new Airplane(eagle);
+        await Airplane.create(hawkAirplane);
+        await Airplane.create(eagleAirplane);
+        const findbefore = await Airplane.find({}, {
+            consistency: SearchConsistency.LOCAL
+        });
+        assert.strictEqual(findbefore.rows.length, 2);
+
+        // remove all docs with name: Couchbase
+        await Airplane.removeMany({
+            name: 'should not find any'
         },
         {
             consistency: SearchConsistency.LOCAL
