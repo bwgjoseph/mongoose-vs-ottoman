af026cb63150474c6af2ca29f8b2803e6d4d7505
bwgjoseph <josephgan@live.com.sg>
2021-01-17 18:00:40 +0800

    update replace and updateById
    Reference #42

--- test/ts/replace.test.ts
+++ test/ts/replace.test.ts
@@ -30,12 +30,15 @@ describe('test replaceById function', async () => {
             },
             },
             created.id);
+        assert.strictEqual(change.callsign, 'abc');
+        assert.strictEqual(change.name, 'ABC Airlines');
         const find = await Airplane.find(
             {},
             {
                 consistency: SearchConsistency.LOCAL
             });
         assert.strictEqual(find.rows[0].callsign, change.callsign);
+        assert.strictEqual(find.rows[0].name, change.name);
 
         await removeDocuments();
     });
@@ -51,17 +54,45 @@ describe('test replaceById function', async () => {
         assert.strictEqual(findbefore.rows.length, 1);
         assert.strictEqual(findbefore.rows[0].callsign, 'Hawk');
 
-        const change = await Airplane.replace({
-            callsign: 'abc',
-            },
-            created.id);
-        const find = await Airplane.find(
-            {},
+        try {
+            await Airplane.replace({
+                callsign: 'abc',
+                },
+                created.id);
+        } catch (error) {
+            assert.strictEqual(error.message, 'Property name is required, Property model is required');
+        }
+
+        await removeDocuments();
+    });
+
+    it('ottoman - should not replace doc when id does not match', async () => {
+        const Airplane = getOttomanModel();
+        const hawkAirplane = new Airplane(hawk);
+        await Airplane.create(hawkAirplane);
+        const findbefore = await Airplane.find({},
             {
                 consistency: SearchConsistency.LOCAL
             });
-        assert.strictEqual(find.rows[0].callsign, change.callsign);
+        assert.strictEqual(findbefore.rows.length, 1);
+        assert.strictEqual(findbefore.rows[0].callsign, 'Hawk');
 
-        await removeDocuments();
+        const change = await Airplane.replace({
+            callsign: 'abc',
+            name: 'ABC Airlines',
+            model: 'A380',
+            location: {
+                type: 'Point',
+                coordinates: [
+                    1.22,
+                    2.33,
+                    1.11
+                ]
+            },
+            },
+            'nosuchid');
+        console.log(change);
+
+        // await removeDocuments();
     });
 })
--- test/ts/updateById.test.ts
+++ test/ts/updateById.test.ts
@@ -29,4 +29,17 @@ describe('test updateById function', async () => {
 
         await removeDocuments();
     });
+
+    it('ottoman - should NOT update doc when id does not match', async () => {
+        const Airplane = getOttomanModel();
+        const hawkAirplane = new Airplane(hawk);
+        await Airplane.create(hawkAirplane);
+        try {
+            await Airplane.updateById('nosuchid', { callsign: 'abc' });
+        } catch (error) {
+            assert.strictEqual(error.message, 'document not found');
+        }
+
+        await removeDocuments();
+    });
 })
