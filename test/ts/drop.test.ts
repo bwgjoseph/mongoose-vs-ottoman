56e4325fa123c50fa7a6b94cdeed2eb5dd43e9aa
bwgjoseph <josephgan@live.com.sg>
2021-02-06 21:13:37 +0800

    update drop test

--- test/ts/drop.test.ts
+++ test/ts/drop.test.ts
@@ -1,57 +1,34 @@
-import assert from 'assert';
-import { Ottoman, Schema, SearchConsistency } from 'ottoman';
-import { removeDocuments } from './setup/util';
+import { Ottoman, SearchConsistency } from 'ottoman';
 
 let ottoman: Ottoman;
 
 const initOttoman = async (searchConsistency: SearchConsistency = SearchConsistency.NONE) => {
-    ottoman = new Ottoman({ scopeName: 'testScope', collectionName: 'testCollection', searchConsistency });
-    assert.strictEqual(ottoman.config.searchConsistency, searchConsistency);
+    ottoman = new Ottoman({ scopeName: 'testDropScope', collectionName: 'testDropCollection', searchConsistency });
 
     ottoman.connect({
         connectionString: 'couchbase://localhost',
-        bucketName: 'testBucket',
+        bucketName: 'testDropBucket',
         username: 'user',
         password: 'password'
     });
 
-    // how to drop the bucket/scope/collection?
-    // how to remove all docs from a bucket/scope/collection?
     await ottoman.start();
-
-    await removeDocuments();
 }
 
-const schema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    operational: {
-        type: Boolean,
-        default: true
-    },
-});
-
-const opt = {
-    name: 'hello',
-    operational: true,
-};
-
-describe('test drop scope', async () => {
+describe('test drop bucket, scope and collection', async () => {
     before(async () => {
         await initOttoman();
     });
 
     it('ottoman - should drop collection', async () => {
-        await ottoman.dropCollection('testCollection', 'testScope');
+        await ottoman.dropCollection('testDropCollection', 'testDropScope');
     });
 
     it('ottoman - should drop scope', async () => {
-        await ottoman.dropScope('testScope');
+        await ottoman.dropScope('testDropScope');
     });
 
     it('ottoman - should drop bucket', async () => {
-        await ottoman.dropBucket('testBucket');
+        await ottoman.dropBucket('testDropBucket');
     });
 });
