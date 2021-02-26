452360a02e4f59fd86020b76b06ce24612b98e80
bwgjoseph <josephgan@live.com.sg>
2021-02-27 01:25:17 +0800

    add changeIdKeyGlobal.test
    Close #48

---
+++ test/ts/changeIdKeyGlobal.test.ts
@@ -0,0 +1,61 @@
+import assert from 'assert';
+import { Ottoman, Schema, SearchConsistency } from 'ottoman';
+import { removeDocuments } from './setup/util';
+
+let ottoman: Ottoman;
+
+const initOttoman = async (searchConsistency: SearchConsistency = SearchConsistency.NONE) => {
+    ottoman = new Ottoman({ collectionName: '_default', idKey: '_id', searchConsistency });
+    assert.strictEqual(ottoman.config.searchConsistency, searchConsistency);
+
+    ottoman.connect({
+        connectionString: 'couchbase://localhost',
+        bucketName: 'testBucket',
+        username: 'user',
+        password: 'password'
+    });
+
+    await ottoman.start();
+
+    await removeDocuments();
+}
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    operational: {
+        type: Boolean,
+        default: true
+    },
+});
+
+const opt = {
+    name: 'hello',
+    operational: true,
+};
+
+describe.only('test model options', async () => {
+    before(async () => {
+        await initOttoman(SearchConsistency.LOCAL);
+    });
+
+    it('ottoman - change global idKey', async () => {
+        assert.strictEqual(ottoman.config.searchConsistency, SearchConsistency.LOCAL);
+
+        const Options = ottoman.model('opts', schema );
+        const OptData = new Options(opt);
+
+        const created = await Options.create(OptData);
+        assert.ok(created._id);
+        assert.ok(!created.id);
+
+        const find = await Options.find({});
+        assert.ok(find.rows[0]._id);
+        assert.ok(!find.rows[0].id);
+        assert.strictEqual(find.rows.length, 1);
+
+        await removeDocuments();
+    });
+})
