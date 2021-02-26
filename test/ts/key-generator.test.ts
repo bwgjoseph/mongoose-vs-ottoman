3bb09ec3570ba43e4046f693e35e244d8e840d75
bwgjoseph <josephgan@live.com.sg>
2021-02-26 17:55:50 +0800

    update key generator test
    Close #29

--- test/ts/key-generator.test.ts
+++ test/ts/key-generator.test.ts
@@ -4,94 +4,85 @@ import { SearchConsistency } from 'ottoman';
 import { ModelMetadata } from 'ottoman/lib/types/model/interfaces/model-metadata.interface';
 import { removeDocuments } from './setup/util';
 
-describe.skip('test keyGenerator function', async () => {
+describe('test keyGenerator function', async () => {
     before(async () => {
         await removeDocuments();
     });
 
-    it('should generator custom key', async () => {
-        const keyGenerator = ({ metadata, id }: {
-            metadata: ModelMetadata;
-            id: any;
-        }) => `gen::${metadata.scopeName}--${metadata.collectionName}::${id}`;
-        const idKey = 'customGKey';
+    it('should be able to define custom key-generator', async () => {
+        const keyGenerator = ({ metadata }: { metadata: ModelMetadata }) => `custom::${metadata.scopeName}--${metadata.collectionName}`;
+
         const schema = new ottoman.Schema({
-            id: { type: String, default: () => 'mycustom::id' }});
-        const generator = ottoman.getModel('DummyDate') || ottoman.model('DummyDate', schema, { keyGenerator, idKey });
-        await generator.create(new generator({}));
-        const options = { select: ['meta().id', 'customGKey'], consistency: SearchConsistency.LOCAL }
-        const doc = await generator.find({}, options);
-        assert.strictEqual(doc.rows.length, 1);
-        assert.ok(Object.keys(doc.rows[0]).includes(idKey));
-        assert.ok(Object.keys(doc.rows[0].id === `gen::_default--DummyDate::${doc.id}`));
+            name: { type: String },
+        });
+        const generator = ottoman.model('Generator', schema, { keyGenerator });
+        await generator.create(new generator({ name: 'a' }));
 
-        await removeDocuments();
-    });
-
-    it('gh-#29 p1', async () => {
-        const random = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        const generated = random();
-
-        const keyGenerator = ({ metadata, id }: {
-            metadata: ModelMetadata;
-            id: any;
-        }) => `gen::${metadata.scopeName}--${metadata.collectionName}::${random()}`;
-        const schema = new ottoman.Schema({
-            id: { type: String, default: () => generated }});
-        const generator = ottoman.getModel('DummyDate') || ottoman.model('DummyDate', schema, { keyGenerator });
-        await generator.create(new generator({}));
         const options = { select: ['meta().id', '*'], consistency: SearchConsistency.LOCAL }
         const doc = await generator.find({}, options);
         assert.strictEqual(doc.rows.length, 1);
-        // unable to find the doc
-        await generator.findById(generated);
 
-        await removeDocuments();
+        const doc2 = await generator.findById(doc.rows[0].testBucket.id);
+        assert.ok(doc2);
+        assert.strictEqual(doc2.id, doc.rows[0].testBucket.id);
     });
 
-    it('gh-#29 p2', async () => {
-        const random = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        const generated = random();
+    it('should be able to define custom key-generator with self-defined id', async () => {
+        const keyGenerator = ({ metadata }: { metadata: ModelMetadata }) => `custom::${metadata.scopeName}--${metadata.collectionName}`;
 
-        const keyGenerator = ({ metadata, id }: {
-            metadata: ModelMetadata;
-            id: any;
-        }) => `gen::${metadata.scopeName}--${generated}--${metadata.collectionName}::`;
-        const schema = new ottoman.Schema({
-            id: { type: String, default: () => generated }});
-        const generator = ottoman.getModel('DummyDate') || ottoman.model('DummyDate', schema, { keyGenerator });
-        await generator.create(new generator({}));
-        const options = { select: ['meta().id', '*'], consistency: SearchConsistency.LOCAL }
-        const doc = await generator.find({}, options);
-        assert.strictEqual(doc.rows.length, 1);
-        // it shouldn't be able to find back the doc?
-        await generator.findById(generated);
-
-        await removeDocuments();
-    });
-
-    it('gh-#29 p3', async () => {
-        const g = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        const g1 = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        console.log(g1);
-
-        const keyGenerator = ({ metadata, id }: {
-            metadata: ModelMetadata;
-            id: any;
-        }) => `gen::${metadata.scopeName}--${g1}--${metadata.collectionName}::`;
         const schema = new ottoman.Schema({
             id: { type: String },
             name: { type: String },
         });
-        const generator = ottoman.getModel('DummyDate') || ottoman.model('DummyDate', schema, { keyGenerator });
+        const generator = ottoman.model('Generator1', schema, { keyGenerator });
         await generator.create(new generator({ id: '1', name: 'a' }));
-        // await generator.create(new generator({ id: '2', name: 'b' }));
-        // await generator.create(new generator({ name: 'c' }));
+
         const options = { select: ['meta().id', '*'], consistency: SearchConsistency.LOCAL }
         const doc = await generator.find({}, options);
-        console.log(JSON.stringify(doc, null, 2));
-        console.log(await generator.findById('hello'));
+        assert.strictEqual(doc.rows.length, 1);
 
-       await removeDocuments();
+        const doc2 = await generator.findById(doc.rows[0].testBucket.id);
+        assert.ok(doc2);
+        assert.strictEqual(doc2.id, doc.rows[0].testBucket.id);
+    });
+
+    it('should be able to define custom key-generator with idKey', async () => {
+        const keyGenerator = ({ metadata }: { metadata: ModelMetadata }) => `custom::${metadata.scopeName}--${metadata.collectionName}`;
+
+        const schema = new ottoman.Schema({
+            id: { type: String },
+            name: { type: String },
+        });
+        const generator = ottoman.model('Generator2', schema, { keyGenerator, idKey: 'hihi' });
+        await generator.create(new generator({ id: '1', name: 'a' }));
+
+        const options = { select: ['meta().id', '*'], consistency: SearchConsistency.LOCAL }
+        const doc = await generator.find({}, options);
+        assert.strictEqual(doc.rows.length, 1);
+
+        const doc2 = await generator.findById(doc.rows[0].testBucket.hihi);
+        assert.ok(doc2);
+        assert.strictEqual(doc2.hihi, doc.rows[0].testBucket.hihi);
+    });
+
+    it('should be able to define custom key-generator with idKey and default value', async () => {
+        const random = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        const keyGenerator = ({ metadata }: { metadata: ModelMetadata }) => `custom::${metadata.scopeName}--${metadata.collectionName}`;
+
+        const once = random();
+        const schema = new ottoman.Schema({
+            id: { type: String, default: () => once },
+            name: { type: String },
+        });
+        const generator = ottoman.model('Generator3', schema, { keyGenerator });
+        await generator.create(new generator({ name: 'a' }));
+
+        const options = { select: ['meta().id', '*'], consistency: SearchConsistency.LOCAL }
+        const doc = await generator.find({}, options);
+        assert.strictEqual(doc.rows.length, 1);
+
+        const doc2 = await generator.findById(once);
+        assert.ok(doc2);
+        assert.strictEqual(doc2.id, once);
     });
 })
