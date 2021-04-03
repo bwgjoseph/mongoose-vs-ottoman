f5375ded922e46da71d3c4445b1bda4270b98bfd
bwgjoseph <josephgan@live.com.sg>
2021-04-03 14:34:32 +0800

    add test case for registerGlobalPlugin
    Reference #50

---
+++ test/ts/registerGlobalPlugin.test.ts
@@ -0,0 +1,58 @@
+import { assert } from 'chai';
+import { Ottoman, registerGlobalPlugin, Schema, SearchConsistency } from 'ottoman';
+import { removeDocuments } from './setup/util';
+
+const pluginLog = (schema: any) => {
+    schema.pre('save', function (doc: any) {
+        doc.plugin = 'registered from plugin!'
+        console.log('[plugin] doc', doc);
+    });
+};
+
+registerGlobalPlugin(pluginLog);
+
+let ottoman: Ottoman;
+
+const initOttoman = async (searchConsistency: SearchConsistency = SearchConsistency.NONE) => {
+    ottoman = new Ottoman({ scopeName: 'testScope123', collectionName: 'testCollection', searchConsistency });
+
+    ottoman.connect({
+        connectionString: 'couchbase://localhost',
+        bucketName: 'testBucket',
+        username: 'user',
+        password: 'password'
+    });
+
+    await ottoman.start();
+
+    await removeDocuments();
+}
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    operational: {
+        type: Boolean,
+        default: true
+    },
+    plugin: String,
+})
+
+describe('test manage bucket, scope and collection', async () => {
+    before(async () => {
+        await initOttoman(SearchConsistency.LOCAL);
+    });
+
+    it('ottoman - should create scope and collection', async () => {
+        const MySchema = ottoman.model('myschema', schema, { idKey: '_id' });
+        const schemaData = new MySchema({
+            name: 'hello',
+            operational: true,
+        });
+
+        const doc = await MySchema.create(schemaData);
+        assert.strictEqual(doc.plugin, 'registered from plugin!');
+    });
+});
