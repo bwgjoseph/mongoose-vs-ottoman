41c2d32868369d33d924036ad0785f728378512a
edwin <edwin-lee@hotmail.com>
2021-05-28 15:50:23 +0800

    Update ttl.mongoose.test.ts

--- test/ts/ttl.mongoose.test.ts
+++ test/ts/ttl.mongoose.test.ts
@@ -3,12 +3,12 @@ import { hawk } from './setup/fixtures';
 import { getMongooseModel } from './setup/model';
 import { assertAirline, removeDocuments } from './setup/util';
 
-describe.only('test create function', async () => {
+describe('test ttl function for mongoose', async () => {
     before(async () => {
         await removeDocuments();
     });
 
-    it.only('mongoose - should create new doc', async () => {
+    it('mongoose - should create new doc then by using ttl, delete the doc after 5 secs', async () => {
         const Airplane = getMongooseModel();
         const hawkAirplane = new Airplane({ ...hawk, createdAt: new Date('21 May 2021 11:30'), });
 
@@ -19,9 +19,9 @@ describe.only('test create function', async () => {
         assert.strictEqual(find.length, 1);
         console.log(find);
 
-        //find db again after 5 secs
+        //find db again after 6 secs, gave 1 more sec as buffer
         await new Promise(resolve => setTimeout(resolve, 6000));
-        console.log('wait for 5 sec');
+        console.log('wait for 6 sec');
         const findAfter5 = await Airplane.find({}).exec();
         assert.strictEqual(findAfter5.length, 0);
         console.log(findAfter5);
