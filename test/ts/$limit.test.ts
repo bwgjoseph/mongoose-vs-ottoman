9f822d4c5320eb380a5939e368b6e80d768d15b6
bwgjoseph <josephgan@live.com.sg>
2021-06-06 01:33:09 +0800

    add negative limit test
    Close #91

--- test/ts/$limit.test.ts
+++ test/ts/$limit.test.ts
@@ -43,6 +43,24 @@ describe('test $limit function', async () => {
         await Airplane.remove({}).exec();
     });
 
+    it('mongoose - negative limit', async () => {
+        const Airplane = getMongooseModel();
+        const hawkAirplane = new Airplane(hawk);
+        const eagleAirplane = new Airplane(eagle);
+        const vultureAirplane = new Airplane(vulture);
+        await Airplane.create(eagleAirplane);
+        await Airplane.create(hawkAirplane);
+        await Airplane.create(vultureAirplane);
+
+        // https://docs.mongodb.com/manual/reference/method/cursor.limit/#negative-values
+        const limitResult = await Airplane.find({
+             operational: true
+        }).limit(-2).exec();
+        assert.ok(limitResult.length === 2);
+
+        await Airplane.remove({}).exec();
+    });
+
     it('ottoman - limit', async () => {
         const Airplane = getOttomanModel();
         const hawkAirplane = new Airplane(hawk);
@@ -94,6 +112,39 @@ describe('test $limit function', async () => {
         const queryFind = await getDefaultInstance().cluster.query(query, { consistency: 'request_plus' });
 
         assert.ok(queryFind.rows.length === 0, 'expected to have 0 result, with limit 0');
+
+        await removeDocuments();
+    });
+
+    it('ottoman - negative limit (#91)', async () => {
+        const Airplane = getOttomanModel();
+        const hawkAirplane = new Airplane(hawk);
+        const eagleAirplane = new Airplane(eagle);
+        const vultureAirplane = new Airplane(vulture);
+        await Airplane.create(eagleAirplane);
+        await Airplane.create(hawkAirplane);
+        await Airplane.create(vultureAirplane);
+
+        const f = await Airplane.find({}, { consistency: SearchConsistency.LOCAL });
+        assert.ok(f.rows.length === 3);
+
+        const find = await Airplane.find(
+            {},
+            {
+                limit: -10,
+                consistency: SearchConsistency.LOCAL
+            },
+        );
+
+        assert.ok(find.rows.length === 0, 'expected to have 0 result, with limit -1');
+
+        // using query should also returns the same result
+        const query = `
+            SELECT * FROM \`testBucket\` LIMIT -10
+            `;
+        const queryFind = await getDefaultInstance().cluster.query(query, { consistency: 'request_plus' });
+
+        assert.ok(queryFind.rows.length === 0, 'expected to have 0 result, with limit -1');
 
         await removeDocuments();
     });
