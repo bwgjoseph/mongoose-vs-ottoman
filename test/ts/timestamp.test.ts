5d0b07511154eea59f7f190291b4854b4a481e2c
bwgjoseph <josephgan@live.com.sg>
2021-09-26 22:25:16 +0800

    add timestamp option test
    Close #89

---
+++ test/ts/timestamp.test.ts
@@ -0,0 +1,50 @@
+import { assert } from 'chai';
+import { getModel, model, Ottoman, Schema, SearchConsistency } from 'ottoman';
+
+let ottoman: Ottoman;
+
+const initOttoman = async (consistency: SearchConsistency = SearchConsistency.NONE) => {
+    ottoman = new Ottoman({ collectionName: '_default', consistency });
+
+    await ottoman.connect({
+        connectionString: 'couchbase://localhost',
+        bucketName: 'testBucket',
+        username: 'user',
+        password: 'password'
+    });
+
+    const timestampSchema = new Schema({ name: String }, { timestamps: true });
+    model('tsModel', timestampSchema);
+
+    const timestampCustomSchema = new Schema({ name: String }, { timestamps: { createdAt: 'ca', updatedAt: 'ua' } });
+    model('tscModel', timestampCustomSchema);
+
+    await ottoman.start();
+}
+
+/**
+ * To run standalone
+ */
+describe('test timestamp feature', async () => {
+    before(async () => {
+        await initOttoman();
+    });
+
+    it('ottoman - should activate timestamp option', async () => {
+        const Model = getModel('tsModel');
+        const created = await Model.create({ name: 'hello' });
+
+        assert.isNotNull(created.createdAt);
+        assert.isNotNull(created.updatedAt);
+        assert.strictEqual(created.name, 'hello');
+    });
+
+    it('ottoman - should activate custom timestamp option', async () => {
+        const Model = getModel('tscModel');
+        const created = await Model.create({ name: 'hello' });
+
+        assert.isNotNull(created.ca);
+        assert.isNotNull(created.ua);
+        assert.strictEqual(created.name, 'hello');
+    });
+});
